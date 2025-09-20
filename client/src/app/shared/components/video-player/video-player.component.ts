52b00e5058af4735c4c11526d889977abaad440b
Ale-dev <acorrea7412@gmail.com>
2025-09-20 02:35:28 -0500

    refactor:  reorganize the code, also add audio extraction and thumbnail generation

--- client/src/app/shared/components/video-player/video-player.component.ts
+++ client/src/app/shared/components/video-player/video-player.component.ts
@@ -1,8 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { AudioService } from '@app/core/services/audio.service';
+import { MediaProcessingService } from '@app/core/services/media-processing.service';
+import { VideoPlayerService } from '@app/core/services/video-player.service';
+import { finalize, forkJoin, Subject, takeUntil } from 'rxjs';
 
-export const DEFAULT_VIDEO_HEIGHT = 622;
-export const DEFAULT_ASPECT_RATIO = '16:9';
+import {
+  DEFAULT_ASPECT_RATIO,
+  DEFAULT_VIDEO_HEIGHT,
+} from '@app/shared/constants/const';
+import { VideoRatio } from '@app/shared/models/type';
 
 @Component({
   selector: 'app-video-player',
@@ -10,42 +23,104 @@ export const DEFAULT_ASPECT_RATIO = '16:9';
   templateUrl: './video-player.component.html',
   styleUrl: './video-player.component.css',
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements AfterViewInit, OnDestroy {
   public videoHeight: number = DEFAULT_VIDEO_HEIGHT;
-  public videoRatio: string = DEFAULT_ASPECT_RATIO;
-  public isVideoLoaded = false;
+  public videoRatio: VideoRatio = DEFAULT_ASPECT_RATIO;
+  public isVideoLoaded: boolean = false;
 
-  @ViewChild('videoContainer') videoContainer!: ElementRef<HTMLElement>;
-  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
+  private destroy$ = new Subject<void>();
 
-  changeVideoAspectRatio(ratio: string) {
-    this.videoRatio = ratio;
+  @ViewChild('videoContainer', { static: false })
+  videoContainer!: ElementRef<HTMLElement>;
+  @ViewChild('videoPlayer', { static: false })
+  videoPlayer!: ElementRef<HTMLVideoElement>;
+
+  constructor(
+    private videoPlayerService: VideoPlayerService,
+    private mediaProcessingService: MediaProcessingService,
+    private audioService: AudioService
+  ) {
+    this.handleSubscriptions();
   }
 
-  selectedFile(event: Event) {
+  private handleSubscriptions(): void {
+    this.videoPlayerService.getAspectRatio().subscribe((aspect) => {
+      this.videoRatio = aspect;
+    });
+  }
+
+  public selectedFile(event: Event): void {
     const fileList = (event.target as HTMLInputElement).files;
     if (!fileList) {
       return;
     }
     const file = fileList[0];
-    if (file.type != 'video/mp4') {
+    if (!this.isValidFile(file)) {
       return;
     }
     this.loadVideo(file);
   }
 
-  loadVideo(file: File) {
+  private loadVideo(file: File): void {
     const videoEl = this.videoPlayer.nativeElement;
     if (videoEl.src) {
       URL.revokeObjectURL(videoEl.src);
+      videoEl.src = '';
     }
     const newUrl = URL.createObjectURL(file);
-    videoEl.src = newUrl;
-    videoEl.load();
+    this.videoPlayerService.setLoading(true);
 
-    videoEl.onloadedmetadata = () => {
-      videoEl.play();
-      this.isVideoLoaded = true;
-    };
+    this.playStreamVideo(newUrl);
+    this.generateVideoAssets(file);
+  }
+
+  private isValidFile(file: File): boolean {
+    const allowedTypes = ['video/mp4', 'video/quicktime'];
+    return allowedTypes.includes(file.type);
+  }
+
+  private playStreamVideo(url: string) {
+    this.videoPlayerService
+      .playStream(url)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {},
+        error: (err) => {
+          URL.revokeObjectURL(url);
+        },
+      });
+  }
+
+  private generateVideoAssets(file: File): void {
+    forkJoin({
+      imagen: this.mediaProcessingService.getVideoThumbnails(file),
+      audio: this.mediaProcessingService.extractAudio(file),
+    })
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(() => {
+          this.videoPlayerService.setLoading(false);
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          this.videoPlayerService.setThumbails(res.imagen);
+          this.audioService.loadAudio(res.audio);
+          this.isVideoLoaded = true;
+        },
+        error: (err) => {
+          this.videoPlayerService.cleanUp();
+          this.isVideoLoaded = false;
+        },
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.videoPlayerService.initializeVideo(this.videoPlayer.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
